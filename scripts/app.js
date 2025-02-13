17128462760631cc6697bfe184ce27268639ad63
Kass-S <kassiespacek101@gmail.com>
2025-02-13 13:40:10 -0800

    table update

--- scripts/app.js
+++ scripts/app.js
@@ -9,21 +9,40 @@ let ageSortBtn = document.getElementById("ageSortBtn");
 let tableBodyText = document.getElementById("tableBodyText");
 
 
-const ShowTable = async () => {
+const SortById = async () => {
     let data = await FetchJson();
 
-    let trContainer = document.createElement('tr');
+    data.forEach(person => {
+        let trContainer = document.createElement('tr');
+
+        trContainer.innerHTML = `<th scope="row" class="px-6 py-4"> ${person.Id} </th> <td class="px-6 py-4"> ${person.FirstName} </td> <td class="px-6 py-4"> ${person.LastName} </td> <td class="px-6 py-4"> ${person.Height} </td> <td class="px-6 py-4"> ${person.Age} </td>` 
+        trContainer.className = 'bg-white dark:bg-gray-800';
+    
+        tableBodyText.appendChild(trContainer);
+    });   
+}
+
+const SortByFirstName = async () => {
+    let data = await FetchJson();
+    console.log(data);
+    
 
     data.forEach(person => {
+        let trContainer = document.createElement('tr');
+
         trContainer.innerHTML = `<th scope="row" class="px-6 py-4"> ${person.Id} </th> <td class="px-6 py-4"> ${person.FirstName} </td> <td class="px-6 py-4"> ${person.LastName} </td> <td class="px-6 py-4"> ${person.Height} </td> <td class="px-6 py-4"> ${person.Age} </td>` 
         trContainer.className = 'bg-white dark:bg-gray-800';
     
         tableBodyText.appendChild(trContainer);
     });
-   
 }
 
 
 idSortBtn.addEventListener('click', () => {
-    ShowTable();
+    SortById();
+})
+
+
+firstNameSortBtn.addEventListener('click', () => {
+    SortByFirstName();
 })
